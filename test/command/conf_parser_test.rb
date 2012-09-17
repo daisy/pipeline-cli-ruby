dde6822db5344e7465676f7ac8ec1125a3043631
Javier Asensio <capitan.cambio@gmail.com>
2012-09-17 16:21:00 +0100

    One test suite to gather them all

--- test/command/conf_parser_test.rb
+++ test/command/conf_parser_test.rb
@@ -7,37 +7,43 @@ require "core/ctxt"
 class TestConfParser< Test::Unit::TestCase
 	def setup
 		Ctxt.conf("config.yml")
+		Ctxt.conf[Conf::PORT]=8181
 	end
+	def teardown
+		
+		Ctxt.conf[Conf::PORT]=8181
+	end
+
 	def test_clean
 		parser=ConfParser.new
-		args=["cosa","-l","--debug","true"]
+		args=["cosa","-l","--port","8080"]
 		clean=parser.cleanSwitches(args)
 
 		assert_not_nil clean.find_index("cosa")
 		assert_not_nil clean.find_index("+l")
-		assert_not_nil clean.find_index("--debug")
-		assert_not_nil clean.find_index("true")
+		assert_not_nil clean.find_index("--port")
+		assert_not_nil clean.find_index("8080")
 		
 	end
 	def test_revert
 		parser=ConfParser.new
-		args=["cosa","+l","--debug","true"]
+		args=["cosa","+l","--port","8080"]
 		clean=parser.revertSwitches(args)
 
 		assert_not_nil clean.find_index("cosa")
 		assert_not_nil clean.find_index("-l")
-		assert_not_nil clean.find_index("--debug")
-		assert_not_nil clean.find_index("true")
+		assert_not_nil clean.find_index("--port")
+		assert_not_nil clean.find_index("8080")
 		
 	end
 	def test_parse
 		parser=ConfParser.new
-		args=["cosa","-l","--debug","true"]
-		Ctxt.conf[Conf::DEBUG]="false"	
+		args=["cosa","-l","--port","8080"]
+		Ctxt.conf[Conf::PORT]="8181"	
 		Ctxt.conf.update_vals
-		assert_equal Ctxt.conf[Conf::DEBUG],"false"
+		assert_equal Ctxt.conf[Conf::PORT],"8181"
 		parser.parse(args)
-		assert_equal Ctxt.conf[Conf::DEBUG],"true"
+		assert_equal Ctxt.conf[Conf::PORT],"8080"
 
 		
 	end
